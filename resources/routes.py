339b84e02e50b948f7fc0775ba2a5273508b277d
Tharusha Dilmith <tharusha@imperiallearning.co.uk>
2022-09-15 09:01:46 +0530

    first commit

--- resources/routes.py
+++ resources/routes.py
@@ -1,4 +1,4 @@
-from .featureDetection import FeatureDetaction
+
 from .setData import DataSet
 from .trainModel import TrainData
 from .handIdentification import HandIdentifier
@@ -7,4 +7,7 @@ def initialize_routes(api):
     # api.add_resource(FeatureDetaction, "/api/face-detect")
     api.add_resource(DataSet, "/api/create-dataset")
     api.add_resource(TrainData, "/api/train-model")
-    api.add_resource(HandIdentifier, "/api/pose-identifier")
+    # api.add_resource(HandIdentifier, "/api/pose-identifier")
+
+    # post api
+    api.add_resource(HandIdentifier, "/api/pose-identifier", methods=['POST'])
